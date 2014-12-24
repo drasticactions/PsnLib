4847b3f15ae4ca9563d014eb9173d4ef0dd6194f
Tim Miller <innerlogic4321@gmail.com>
2014-12-23 20:47:00 -0500

    Update WebManager

--- PsnLib/Managers/WebManager.cs
+++ PsnLib/Managers/WebManager.cs
@@ -28,10 +28,19 @@ namespace PsnLib.Managers
                     var language = userAccountEntity.GetUserEntity().Language;
                     httpClient.DefaultRequestHeaders.Add("Accept-Language", language);
                 }
+
                 httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", userAccountEntity.GetAccessToken());
-                var response = await httpClient.PostAsync(uri, header);
+                HttpResponseMessage response;
+                if (header == null)
+                {
+                    response = await httpClient.PostAsync(uri, content);
+                }
+                else
+                {
+                    response = await httpClient.PostAsync(uri, header);
+                }
                 var responseContent = await response.Content.ReadAsStringAsync();
-                return string.IsNullOrEmpty(responseContent) ? new Result(false, string.Empty) : new Result(true, responseContent);
+                return new Result(true, responseContent);
             }
             catch
             {
@@ -40,14 +49,59 @@ namespace PsnLib.Managers
             }
         }
 
-        public Task<Result> PutData(Uri uri, StringContent json, UserAccountEntity userAccountEntity)
+        public async Task<Result> PutData(Uri uri, StringContent json, UserAccountEntity userAccountEntity)
         {
-            throw new NotImplementedException();
+            var httpClient = new HttpClient();
+            try
+            {
+                var authenticationManager = new AuthenticationManager();
+                if (userAccountEntity.GetAccessToken().Equals("refresh"))
+                {
+                    await authenticationManager.RefreshAccessToken(userAccountEntity);
+                }
+                var user = userAccountEntity.GetUserEntity();
+                if (user != null)
+                {
+                    var language = userAccountEntity.GetUserEntity().Language;
+                    httpClient.DefaultRequestHeaders.Add("Accept-Language", language);
+                }
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", userAccountEntity.GetAccessToken());
+                var response = await httpClient.PutAsync(uri, json);
+                var responseContent = await response.Content.ReadAsStringAsync();
+                return response.IsSuccessStatusCode ? new Result(true, string.Empty) : new Result(false, responseContent);
+            }
+            catch (Exception)
+            {
+
+                return new Result(false, string.Empty);
+            }
         }
 
-        public Task<Result> DeleteData(Uri uri, StringContent json, UserAccountEntity userAccountEntity)
+        public async Task<Result> DeleteData(Uri uri, StringContent json, UserAccountEntity userAccountEntity)
         {
-            throw new NotImplementedException();
+            var httpClient = new HttpClient();
+            try
+            {
+                var authenticationManager = new AuthenticationManager();
+                if (userAccountEntity.GetAccessToken().Equals("refresh"))
+                {
+                    await authenticationManager.RefreshAccessToken(userAccountEntity);
+                }
+                var user = userAccountEntity.GetUserEntity();
+                if (user != null)
+                {
+                    var language = userAccountEntity.GetUserEntity().Language;
+                    httpClient.DefaultRequestHeaders.Add("Accept-Language", language);
+                }
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", userAccountEntity.GetAccessToken());
+                var response = await httpClient.DeleteAsync(uri);
+                var responseContent = await response.Content.ReadAsStringAsync();
+                return response.IsSuccessStatusCode ? new Result(true, string.Empty) : new Result(false, responseContent);
+            }
+            catch (Exception)
+            {
+                return new Result(false, string.Empty);
+            }
         }
 
         public async Task<Result> GetData(Uri uri, UserAccountEntity userAccountEntity)
